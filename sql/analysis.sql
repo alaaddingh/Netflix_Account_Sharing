8acae512792e53d8195da67a2c0f020d4ac0486b
alaaddingh <alagho03@gmail.com>
2026-02-27 18:52:07 -0800

    queries

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,12 +1,39 @@
+-- GLOBAL ARPA for 2022 and 2023 --
+SELECT
+    YEAR(Date) AS Year,
+    SUM(CAST(Revenue AS FLOAT)) / SUM(Average_Paying_Memberships) / 3.0 AS Global_ARPA
+FROM dbo.QuarterlyEarnings
+WHERE YEAR(Date) IN (2022, 2023)
+GROUP BY YEAR(Date)
+ORDER BY Year;
 
--- time to do some data exploration!! --
-SELECT * FROM dbo.QuarterlyEarnings;
 
-
--- 1: How did Revenue Change quarter to quarter Globally? (keep in mind, change occurred globally in may 2023) --
-
-SELECT Date, SUM(Revenue) as Total_Revenue, 
+-- Global Subscribers and Revenue QoQ ---
+SELECT 
+    Date,
+    SUM(CAST(Revenue AS FLOAT)) * 1000 AS Global_Revenue, 
+    SUM(CAST(Average_Paying_Memberships AS FLOAT)) * 1000 AS Global_Memberships
 FROM dbo.QuarterlyEarnings
 GROUP BY Date
 ORDER BY Date;
 
+
+
+-- 2023 Global QoQ Revenue and Memberships --
+SELECT 
+    Date,
+    SUM(CAST(Revenue AS FLOAT)) * 1000 AS Global_Revenue, 
+    SUM(CAST(Average_Paying_Memberships AS FLOAT)) * 1000 AS Global_Memberships
+FROM dbo.QuarterlyEarnings
+WHERE YEAR(Date) = 2023
+GROUP BY Date
+ORDER BY Date;
+
+-- Annual ARPA Per Region --
+SELECT
+    YEAR(Date) AS Year,
+    Location,
+    SUM(CAST(Revenue AS FLOAT)) / SUM(Average_Paying_Memberships) / 3.0 AS ARPA
+FROM dbo.QuarterlyEarnings
+GROUP BY YEAR(Date), Location
+ORDER BY Year, Location;
